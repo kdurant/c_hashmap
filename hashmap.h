073213f165a1c912816493015322c31494aee7b5
kdurant <wangjun850725@163.com>
2019-08-25 13:58:17 +0800

    add makefile

--- hashmap.h
+++ hashmap.h
@@ -9,16 +9,16 @@
 #ifndef __HASHMAP_H__
 #define __HASHMAP_H__
 
-#define MAP_MISSING -3  /* No such element */
-#define MAP_FULL -2 	/* Hashmap is full */
-#define MAP_OMEM -1 	/* Out of Memory */
-#define MAP_OK 0 	/* OK */
+#define MAP_MISSING -3 /* No such element */
+#define MAP_FULL -2    /* Hashmap is full */
+#define MAP_OMEM -1    /* Out of Memory */
+#define MAP_OK 0       /* OK */
 
 /*
  * any_t is a pointer.  This allows you to put arbitrary structures in
  * the hashmap.
  */
-typedef void *any_t;
+typedef void* any_t;
 
 /*
  * PFany is a pointer to a function that can take two any_t arguments
@@ -55,7 +55,7 @@ extern int hashmap_put(map_t in, char* key, any_t value);
 /*
  * Get an element from the hashmap. Return MAP_OK or MAP_MISSING.
  */
-extern int hashmap_get(map_t in, char* key, any_t *arg);
+extern int hashmap_get(map_t in, char* key, any_t* arg);
 
 /*
  * Remove an element from the hashmap. Return MAP_OK or MAP_MISSING.
@@ -66,7 +66,7 @@ extern int hashmap_remove(map_t in, char* key);
  * Get any element. Return MAP_OK or MAP_MISSING.
  * remove - should the element be removed from the hashmap
  */
-extern int hashmap_get_one(map_t in, any_t *arg, int remove);
+extern int hashmap_get_one(map_t in, any_t* arg, int remove);
 
 /*
  * Free the hashmap
@@ -78,4 +78,4 @@ extern void hashmap_free(map_t in);
  */
 extern int hashmap_length(map_t in);
 
-#endif __HASHMAP_H__
+#endif
